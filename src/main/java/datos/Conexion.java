6cb106f9fc70eacafc2d5f2301c47b15a3cb57c6
Carlos P. <dawcarlospereira@gmail.com>
2026-02-02 12:25:43 +0100

    Subiendo el proyecto inicial

---
+++ src/main/java/datos/Conexion.java
@@ -0,0 +1,13 @@
+
+
+package datos;
+
+/**
+ *
+ * @author cpere
+ */
+public class Conexion {
+    private static final String JDBC_URL = 
+            "jdbc:mysql://localhost:3306/control_clientes?"
+            + "useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+}
